1bbbbc66f705860aea39255b72f920a42b7b75f4
Gandalf1209 (Jonah) <criminal.jba@gmail.com>
2016-06-22 23:19:01 -0500

    Commit #0003
    Keyboard implementation
    Keyboard codes copied from LWJGL

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,7 @@
 package test;
 
 import com.amittaigames.lgl3.CoreGame;
+import com.amittaigames.lgl3.Keys;
 import com.amittaigames.lgl3.Plane;
 import com.amittaigames.lgl3.Render;
 import com.amittaigames.lgl3.Window;
@@ -8,6 +9,7 @@ import com.amittaigames.lgl3.Window;
 public class Test extends CoreGame {
 
 	private Plane plane;
+	private float speed = 3.5f;
 	
 	public static void main(String[] args) {
 		Window.init("Ludum GL 3", 800, 600, new Test());
@@ -27,7 +29,18 @@ public class Test extends CoreGame {
 
 	@Override
 	public void update() {
-		
+		if (Keys.isPressed(Keys.KEY_D)) {
+			plane.translate(speed, 0);
+		}
+		if (Keys.isPressed(Keys.KEY_A)) {
+			plane.translate(-speed, 0);
+		}
+		if (Keys.isPressed(Keys.KEY_W)) {
+			plane.translate(0, -speed);
+		}
+		if (Keys.isPressed(Keys.KEY_S)) {
+			plane.translate(0, speed);
+		}
 	}
 
 }
